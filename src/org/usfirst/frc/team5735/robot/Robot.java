458c28b6f93748740d5508877737518b18a2b545
Alexander Briasco-Stewart <astewart.is1@gmail.com>
2017-02-12 11:38:43 -0500

    adding my actual changes

--- src/org/usfirst/frc/team5735/robot/Robot.java
+++ src/org/usfirst/frc/team5735/robot/Robot.java
@@ -70,13 +70,12 @@ public class Robot extends IterativeRobot {
 		helloKittyStarted = false;
 		seekingPeg = false;
 		encoderDebug = false;
-		
-<<<<<<< HEAD
+
 		driveTrain = new RobotDrive(left1, left2, right1, right2);
-=======
+
 		//driveTrain = new RobotDrive(left1, left2, right1, right2);
 
->>>>>>> Initial Commit
+
 		
 		shooterElevator = new CANTalon(1);
 		shooterElevator.set(0);
@@ -114,7 +113,7 @@ public class Robot extends IterativeRobot {
 	              }
 	              synchronized (imgLock) {
 	            	  //filter code
-	            	  this.boundingRectOutput = this.filterRectsPeg(inputRects);
+	            	  this.boundingRectOutput = Vision.filterRectsPeg(inputRects);
 	              }
 	          }
 	    });
@@ -137,47 +136,6 @@ public class Robot extends IterativeRobot {
 	private static int IMG_HEIGHT = 240;
 	private VisionThread visionThread;
 	private final Object imgLock = new Object();
-	
-	private static ArrayList<Rect> filterRectsPeg(ArrayList<Rect> inputRects) {
-		ArrayList<Rect> output = new ArrayList<Rect>();
-		Rect curBiggest1 = null;
-		Rect curBiggest2 = null;
-		double biggestArea = 0;
-		for (int i = 0; i < inputRects.size(); i++) {
-			for (int j = 0; j < inputRects.size(); j++) {
-				if (j == i) {
-					continue;
-				}
-
-				Rect rA = inputRects.get(i);
-				Rect rB = inputRects.get(j);
-
-				if (rA.area() < 300 || rB.area() < 300) {
-				}
-				if((double)(rA.height) /(double)(rA.width) <1){
-					continue;
-				}
-				if((double)(rB.height) /(double)(rB.width) <1){
-					continue;
-				}
-				if ((Math.abs(rA.area() - rB.area()) <= .2 * rB.area()) && Math.abs(rA.y - rB.y) <= 20) {
-					if(rA.area() + rB.area() > biggestArea){
-						curBiggest1 = rA;
-						curBiggest2 = rB;
-						biggestArea = rA.area() + rB.area();
-					}
-				}
-			}
-		}
-		output.clear();
-		if(curBiggest1 == null && curBiggest2 == null){
-			return output;
-		}
-		output.add(curBiggest1);
-		output.add(curBiggest2);
-		return output;
-
-	}
 
 	@Override
 	public void autonomousInit() {
@@ -235,16 +193,17 @@ public class Robot extends IterativeRobot {
         	//Vision & peg seeking
         	if (driveController.isButtonPressed(XboxController.X_BUTTON)) {
         		if(seekingPeg){
-        			visionThread.stop();
+        			try{
+        				visionThread.sleep(40);
+        			}
+        			catch(InterruptedException e){
+        				
+        			}
         			seekingPeg = false;
         		}
         	}
         	else{
-<<<<<<< HEAD
-        		visionThread.start()
-=======
-        		visionThread.start();
->>>>>>> Initial Commit
+        		visionThread.interrupt();
         		seekingPeg = true;
         	}
 //        		System.out.println("SeekingPeg: " + seekingPeg);
--- src/org/usfirst/frc/team5735/robot/Vision.java
+++ src/org/usfirst/frc/team5735/robot/Vision.java
@@ -1,5 +1,49 @@
 package org.usfirst.frc.team5735.robot;
 
-public class Vision {
+import java.util.ArrayList;
 
+import org.opencv.core.Rect;
+
+public class Vision {
+	
+	public static ArrayList<Rect> filterRectsPeg(ArrayList<Rect> inputRects) {
+		ArrayList<Rect> output = new ArrayList<Rect>();
+		Rect curBiggest1 = null;
+		Rect curBiggest2 = null;
+		double biggestArea = 0;
+		for (int i = 0; i < inputRects.size(); i++) {
+			for (int j = 0; j < inputRects.size(); j++) {
+				if (j == i) {
+					continue;
+				}
+
+				Rect rA = inputRects.get(i);
+				Rect rB = inputRects.get(j);
+
+				if (rA.area() < 300 || rB.area() < 300) {
+				}
+				if((double)(rA.height) /(double)(rA.width) <1){
+					continue;
+				}
+				if((double)(rB.height) /(double)(rB.width) <1){
+					continue;
+				}
+				if ((Math.abs(rA.area() - rB.area()) <= .2 * rB.area()) && Math.abs(rA.y - rB.y) <= 20) {
+					if(rA.area() + rB.area() > biggestArea){
+						curBiggest1 = rA;
+						curBiggest2 = rB;
+						biggestArea = rA.area() + rB.area();
+					}
+				}
+			}
+		}
+		output.clear();
+		if(curBiggest1 == null && curBiggest2 == null){
+			return output;
+		}
+		output.add(curBiggest1);
+		output.add(curBiggest2);
+		return output;
+
+	}
 }
