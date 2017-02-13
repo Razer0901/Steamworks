01134b4ca53af22dc780286c04261938d3514105
Alexander Briasco-Stewart <astewart.is1@gmail.com>
2017-02-13 16:56:57 -0500

    Autonomous finished

--- src/org/usfirst/frc/team5735/robot/Autonomous.java
+++ src/org/usfirst/frc/team5735/robot/Autonomous.java
@@ -30,13 +30,23 @@ public class Autonomous {
 	private Encoder LEncoder, REncoder;
 	private AHRS gyro;
 	private double x, y;
+	private double currAngle, targetAngle;
+	private int seeking;	//0 = NOT seeking, 1 = turning, 2 = moving
+	private double distMoved, distToMove;
+	private RobotDrive driveTrain;
+	//make sure that robot starts at angle = 0;
 	
-	public Autonomous(Encoder left, Encoder right, AHRS ahrs){
+	public Autonomous(Encoder left, Encoder right, AHRS ahrs, RobotDrive dTrain){
 		LEncoder = left;
 		REncoder = right;
 		gyro = ahrs;
 		x = 0;
 		y = 0;
+		currAngle = 0;
+		targetAngle = 0;
+		seeking = 0;
+		distMoved = 0;
+		driveTrain = dTrain;
 	}
 	/**
 	 * Reset the values of the Encoders, Gyro, and current position
@@ -45,9 +55,11 @@ public class Autonomous {
 		LEncoder.reset();
 		REncoder.reset();
 		gyro.reset();
-		
 		x = 0;
 		y = 0;
+		seeking = 0;
+		distMoved = 0;
+		distToMove = 0;
 	}
 	
 	public void moveTo(double xpos, double ypos){
@@ -56,6 +68,39 @@ public class Autonomous {
 		double radAng = Math.atan2(dy,  dx);
 		radAng = (radAng<0) ? radAng + 2*Math.PI : radAng;
 		double degAbs = Math.toDegrees(radAng);
+		targetAngle = degAbs;
+		distMoved = 0;
+		distToMove = Math.sqrt(dx*dx + dy*dy);
+		seeking = 1;
+	}
+	public void update(){
+		if(seeking == 0){
+			return;
+		}
+		currAngle = gyro.getAngle() % 360;
+		if(seeking == 1){
+			if(targetAngle - currAngle > 5.0 ){
+				driveTrain.arcadeDrive(0, 0.5);		//TURN LEFT
+			}
+			else if(targetAngle - currAngle < 5.0){
+				driveTrain.arcadeDrive(0, -0.5);	//TURN RIGHT
+			}
+			else {									//CORRECT ANGLE
+				seeking = 2;
+				LEncoder.reset();
+				REncoder.reset();
+			}
+		}
+		if (seeking == 2){
+			distMoved = (LEncoder.getDistance() + REncoder.getDistance()) /2.0;
+			if(distMoved < distToMove){
+				driveTrain.arcadeDrive(0.5, 0);
+			}
+			else{
+				seeking = 0;
+				driveTrain.arcadeDrive(0, 0);
+			}
+		}
 		
 	}
 	
